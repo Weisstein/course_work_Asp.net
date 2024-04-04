ccc0898ef1f88d01a204a5b6f734b2b38e6255f9
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-04 12:31:17 +0300

    CreteComponentServisesAndServiseInterfase

---
+++ backend/PcBuilder.Core/Abstractions/IComponentServise.cs
@@ -0,0 +1,19 @@
+﻿using PcBuilder.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcBuilder.Core.Abstractions
+{
+    public interface IComponentServise
+    {
+        Task<Guid> Add(Component component);
+        Task<Guid> Delete(Guid id);
+        Task<List<Component>> GetAll();
+        Task<List<Component>> GetByFilter(Guid? typeId, string? name, Guid? charId, string? value);
+        Task<Component> GetById(Guid id);
+        Task<Guid> Update(Guid id, string title, string description, decimal price);
+    }
+}
