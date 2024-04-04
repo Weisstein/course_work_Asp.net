cf04611664e05f6b378e19b542b05dfdeb3d9653
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-04 10:33:23 +0300

    Create ComponentRepository.cs

---
+++ backend/PcBuilder.DAL.MSSQL/Repositories/ComponentRepository.cs
@@ -0,0 +1,21 @@
+﻿using PcBuilder.Core.Models;
+
+namespace PcBuilder.DAL.MySQL.Repositories
+{
+    public class ComponentRepository
+    {
+        private readonly BuilderDBContext _dbContext;
+
+        public ComponentRepository(BuilderDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Component>> GetAll()
+        {
+
+        }
+
+
+    }
+}
