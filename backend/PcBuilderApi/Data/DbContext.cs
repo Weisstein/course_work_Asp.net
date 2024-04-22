d46878a7ddb511b40f3d8bd9ee52a666bd2952d7
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-22 17:53:04 +0300

    Update DbContext.cs

--- backend/PcBuilderApi/Data/DbContext.cs
+++ backend/PcBuilderApi/Data/DbContext.cs
@@ -1,8 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PcBuilderApi.Models;
+using PcBuilderApi.Models.Configs;
 
 namespace PcBuilderApi.Data
 {
     public class DataContext(DbContextOptions<DbContext> options) : DbContext(options)
     {
+        public DbSet<ComponentType> componentTypes { get; set; }
+        public DbSet<Component> components { get; set; }
+        public DbSet<ComponentCharact> charact { get; set; }
+        public DbSet<Build> builds { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ComponentTypeConf());
+            modelBuilder.ApplyConfiguration(new ComponentConf());
+            modelBuilder.ApplyConfiguration(new ComponentCharactConf());
+            modelBuilder.ApplyConfiguration(new BuildConf());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
