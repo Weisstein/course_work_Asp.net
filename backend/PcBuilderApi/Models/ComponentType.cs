fd3e6067b49717eca828ac51e28310848566438f
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-22 17:11:39 +0300

    ComponentTypeModel

--- backend/PcBuilderApi/Models/ComponentType.cs
+++ backend/PcBuilderApi/Models/ComponentType.cs
@@ -1,9 +1,11 @@
-﻿namespace PcBuilderApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PcBuilderApi.Models
 {
     public class ComponentType
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-
+        public ICollection<Component>? Components { get; set; }
     }
 }
---
+++ backend/PcBuilderApi/Models/Configs/ComponentTypeConf.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PcBuilderApi.Models.Configs
+{
+    public class ComponentTypeConf : IEntityTypeConfiguration<ComponentType>
+    {
+        public void Configure(EntityTypeBuilder<ComponentType> builder)
+        {
+           builder.HasKey(ct => ct.Id);
+
+           builder.HasAlternateKey(ct => ct.Name);
+
+           builder.Property(ct => ct.Name)
+                .HasMaxLength(256)
+                .IsRequired();
+        }
+    }
+}
