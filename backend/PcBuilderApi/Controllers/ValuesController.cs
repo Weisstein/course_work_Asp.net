26e39c2018b53d534e1d58daef3b2124ebd77c1d
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-22 19:08:52 +0300

    fdg

---
+++ backend/PcBuilderApi/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PcBuilderApi.Data;
+using PcBuilderApi.Dtos;
+using PcBuilderApi.Models;
+
+namespace PcBuilderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        
+        public ValuesController(DataContext dataContext) {  _dataContext = dataContext; }
+
+        [HttpGet]
+        public async Task<ActionResult<ComponentType>> GetAll()
+        {
+            var componentType = await _dataContext.componentTypes
+                .AsNoTracking()
+                .ToListAsync();
+
+            var response = componentType.Select(ct => new ComponentTypeGet(ct.Id, ct.Name));
+
+            return Ok(response);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ComponentType>> GetById(int id)
+        {
+            var componentType = await _dataContext.componentTypes
+                .AsNoTracking()
+                .FirstOrDefaultAsync(ct => ct.Id == id);
+                
+            if (componentType == null)
+            {
+                return NotFound();
+            }
+
+            var response = new ComponentTypeGet(componentType.Id, componentType.Name);
+
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(ComponentTypePostPut request)
+        {
+            var newType = new ComponentType
+            {
+                Name = request.Name
+            };
+
+            if (string.IsNullOrEmpty(request.Name))
+            {
+                return BadRequest();
+            }
+
+            _dataContext.componentTypes.Add(newType);
+            await _dataContext.SaveChangesAsync();
+
+
+            return Ok();
+        }
+    }
+}
