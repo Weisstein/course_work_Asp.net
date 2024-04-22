4ad7f78becc5a09db6efd9d96fa79c0e770e97f9
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-22 18:23:51 +0300

    Update Program.cs

--- backend/PcBuilderApi/Program.cs
+++ backend/PcBuilderApi/Program.cs
@@ -5,12 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseMySql(connString, ServerVersion.AutoDetect(connString)));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseMySql(connString, ServerVersion.AutoDetect(connString)));
 
 var app = builder.Build();
 
