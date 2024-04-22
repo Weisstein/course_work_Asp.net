4f251775a1b1203c99581ff81fc807c9eab13e26
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-22 18:41:39 +0300

    Squashed commit of the following:
    commit 817d565be864e79a82b37531c592d1da05323b53
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 18:41:17 2024 +0300
    
        fg
    
    commit 726e17613202afb15bc9fa0f3972baa59292ac66
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 18:24:01 2024 +0300
    
        Update PcBuilderApi.csproj
    
    commit 4ad7f78becc5a09db6efd9d96fa79c0e770e97f9
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 18:23:51 2024 +0300
    
        Update Program.cs
    
    commit fae23d69e63ede9069b50f23085f735cf3f050fc
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 18:20:18 2024 +0300
    
        fg
    
    commit 58694fa23cecd7e463b336be35180470e8eb734a
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 18:03:46 2024 +0300
    
        h
    
    commit a4419df095339d0c10fb0c497ddca2ef21554fa7
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 18:00:30 2024 +0300
    
        Delete PcBuilderApi - Backup.csproj
    
    commit d46878a7ddb511b40f3d8bd9ee52a666bd2952d7
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 17:53:04 2024 +0300
    
        Update DbContext.cs
    
    commit 6d97893645234443b1c0df753b83f4c47f6216a1
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 17:49:04 2024 +0300
    
        ConfsAndModels
    
    commit 575bfa07457d2fd72b316e9c3e68b1f685a789d8
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 17:30:15 2024 +0300
    
        Conf
    
    commit fd3e6067b49717eca828ac51e28310848566438f
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 17:11:39 2024 +0300
    
        ComponentTypeModel
    
    commit 1edfe5a25b38691720444e78ccd5bbb1bd72053a
    Author: Anatoly <anatolijmorozow1398@gmail.com>
    Date:   Mon Apr 22 16:58:26 2024 +0300
    
        context

---
+++ backend/PcBuilderApi/Models/Build.cs
@@ -0,0 +1,13 @@
+﻿namespace PcBuilderApi.Models
+{
+    public class Build
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public ICollection<Component>? Components { get; set; }
+    }
+}
---
+++ backend/PcBuilderApi/Models/Configs/ComponentTypeConf.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace PcBuilderApi.Models.Configs
+{
+    public class ComponentTypeConf : IEntityTypeConfiguration<ComponentType>
+    {
+        public void Configure(EntityTypeBuilder<ComponentType> builder)
+        {
+           builder.HasKey(ct => ct.Id);
+
+           builder.Property(ct => ct.Id)
+                  .UseMySqlIdentityColumn();
+
+           builder.HasAlternateKey(ct => ct.Name);
+
+           builder.Property(ct => ct.Name)
+                .HasMaxLength(128)
+                .IsRequired();
+        }
+    }
+}
