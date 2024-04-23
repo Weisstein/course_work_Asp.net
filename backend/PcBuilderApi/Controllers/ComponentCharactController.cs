0efc678e6c9939b39449960343ac4f58ae498281
Anatoly <anatolijmorozow1398@gmail.com>
2024-04-23 16:26:58 +0300

    jkl

---
+++ backend/PcBuilderApi/Controllers/ComponentCharactController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PcBuilderApi.Data;
+using PcBuilderApi.Dtos;
+
+namespace PcBuilderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComponentCharactController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ComponentCharactController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ComponentCharactGet>> GetByComponent(int id)
+        {
+            var charact = await _dataContext.charact
+                .Where(c => c.ComponentId == id)
+                .AsNoTracking()
+                .ToListAsync();
+            
+            if (charact == null)
+            {
+                return NotFound();
+            }
+
+            var response = charact.Select(cc => new ComponentCharactGet(cc.Id,cc.Name,cc.Value));
+            return Ok(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(int id, ComponentCharactPostPut request)
+        {
+            var charact = await _dataContext.charact
+                .AsNoTracking()
+                .FirstOrDefaultAsync(cc => cc.Id == id);
+
+            if (charact == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.charact
+                .ExecuteUpdate(cc => cc
+                .SetProperty(cc => cc.Name, request.Name)
+                .SetProperty(cc => cc.Value, request.Value)
+                );
+        }
+    }
+}
--- backend/PcBuilderApi/Dtos/ComponentCaractDto.cs
+++ backend/PcBuilderApi/Dtos/ComponentCaractDto.cs
@@ -1,5 +1,12 @@
 ﻿namespace PcBuilderApi.Dtos
 {
+    public record struct ComponentCharactGet
+    (
+        int Id,
+        string Name,
+        string Value
+    );
+
     public record struct ComponentCharactPostPut
     (
         string Name,
